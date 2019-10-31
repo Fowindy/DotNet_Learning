f52f7afd3531492f4345a21b5bc0c94ef5c1cf88
Fowindy <470026339@qq.com>
2019-10-31 23:53:30 +0800

    partial部分类本质上是同一个类,所有资源共享;

---
+++ src/DotNet_Basic_Day20/Listing20.02.关键字.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fowindy.DotNet_Basic_Day20.Listing20_02
+{
+    public partial class Person
+    {
+        private string _name;
+    }
+
+    public partial class Person
+    {
+        public void T()
+        {
+            //partial部分类本质上是同一个类,所有资源共享;
+            _name = "可以访问";
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            //partial 部分类
+        }
+    }
+}
