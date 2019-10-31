a006b0fbdd5e5c5dc6627cf065b5db7c97287115
Fowindy <470026339@qq.com>
2019-10-31 18:53:05 +0800

    添加项目文件。

---
+++ DotNet_Basic_Class/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNet_Basic_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
