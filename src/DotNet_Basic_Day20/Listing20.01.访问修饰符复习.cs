50eff048d9c508caa97fbf21edb85c8f1d0aa151
Fowindy <470026339@qq.com>
2019-10-31 19:42:14 +0800

    this和new关键字的作用

---
+++ src/DotNet_Basic_Day20/Listing20.01.访问修饰符复习.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fowindy.DotNet_Basic_Day20.Listing20_01
+{
+    public class Person
+    {
+        public void T()
+        {
+            Console.WriteLine("父类方法");
+        }
+    }
+
+    public class Teacher : Person
+    {
+        //子类存在和父类同名的方法；子类将会屏蔽父类的方法；
+        public new void T()
+        {
+            Console.WriteLine("子类方法");
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            //note1.需要注意的关键字this和new
+            ///<![CDATA[this的作用
+            ///&作用1:指代当前类的对象;
+            ///&作用2:调用自己的构造函数;]]>
+            ///
+
+            ///<![CDATA[new的作用:
+            ///&作用1:创建对象;
+            ///&作用2:隐藏父类的成员;举例说明]]>
+            ///
+
+            Teacher teacher = new Teacher();
+            teacher.T();//父类的方法
+        }
+    }
+}
