0bb30dd3f9d96c18883d852e7d06b9b0051946cc
Fowindy <470026339@qq.com>
2019-11-01 08:32:03 +0800

    抽象类注意3:子类重写父类的抽象方法,必须和父类抽象方法有相同的签名;(传参和返回值的类型个数一致)

--- src/DotNet_Basic_Day20/Listing20.04.接口复习.cs
+++ src/DotNet_Basic_Day20/Listing20.04.接口复习.cs
@@ -5,6 +5,8 @@ using System.Text;
 namespace Fowindy.DotNet_Basic_Day20.Listing20_04
 {
     //抽象类注意1:抽象成员必须在抽象类中;
+    //抽象类注意2:抽象方法不能有任何实现;
+    //抽象类注意3:子类重写父类的抽象方法,必须和父类抽象方法有相同的签名;(传参和返回值的类型个数一致)
     //但如果父类是抽象类
     public abstract class Person
     {
@@ -15,6 +17,8 @@ namespace Fowindy.DotNet_Basic_Day20.Listing20_04
         }
 
         public abstract void Test();
+
+        public abstract void Test1(string name);
     }
 
     public class Student : Person
@@ -23,6 +27,11 @@ namespace Fowindy.DotNet_Basic_Day20.Listing20_04
         public override void Test()
         {
             
+        }
+
+        public override void Test1(string name)//括号中的签名和父类抽象方法保存一致
+        {
+            throw new NotImplementedException();
         }
         //子类可以重写父类中的虚方法,也可以不重写父类中的虚方法
     }
