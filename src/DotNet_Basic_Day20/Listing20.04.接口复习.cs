7ec7aae0a84e4f90e27230071dfa9dd7fca576ce
Fowindy <470026339@qq.com>
2019-11-01 08:20:08 +0800

    父类中如果有方法需要让子类重写,则可以将该方法标记为virtual 虚方法在父类中必须有实现,哪怕是空实现;(有方法体无内容)

---
+++ src/DotNet_Basic_Day20/Listing20.04.接口复习.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fowindy.DotNet_Basic_Day20.Listing20_04
+{
+    public class Person
+    {
+        //父类中如果有方法需要让子类重写,则可以将该方法标记为virtual
+        //虚方法在父类中必须有实现,哪怕是空实现;(有方法体无内容)
+        public virtual void SayHello()
+        {
+        }
+    }
+
+    public class Student : Person
+    {
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
