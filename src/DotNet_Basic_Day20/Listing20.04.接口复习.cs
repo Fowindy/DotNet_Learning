e3d06736e73c0af6045a72347ce290118a64fe5c
Fowindy <470026339@qq.com>
2019-11-01 08:23:09 +0800

    但如果父类是抽象类,则子类必须重写父类中的抽象成员

--- src/DotNet_Basic_Day20/Listing20.04.接口复习.cs
+++ src/DotNet_Basic_Day20/Listing20.04.接口复习.cs
@@ -4,17 +4,25 @@ using System.Text;
 
 namespace Fowindy.DotNet_Basic_Day20.Listing20_04
 {
-    public class Person
+    //但如果父类是抽象类
+    public abstract class Person
     {
         //父类中如果有方法需要让子类重写,则可以将该方法标记为virtual
         //虚方法在父类中必须有实现,哪怕是空实现;(有方法体无内容)
         public virtual void SayHello()
         {
         }
+
+        public abstract void Test();
     }
 
     public class Student : Person
     {
+        //则子类必须重写父类中的抽象成员
+        public override void Test()
+        {
+            
+        }
         //子类可以重写父类中的虚方法,也可以不重写父类中的虚方法
     }
     public class Program
