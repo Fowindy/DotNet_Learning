9e8f69e06e4000dc5d3944bee1c007f78d1fdba3
Fowindy <470026339@qq.com>
2019-11-02 08:03:07 +0800

    不能在父类中声明Fly()的方法用于子类继承;因为不是所有的鸟都会飞

--- src/DotNet_Basic_Day20/Listing20.05.接口复习.cs
+++ src/DotNet_Basic_Day20/Listing20.05.接口复习.cs
@@ -10,12 +10,28 @@ namespace Fowindy.DotNet_Basic_Day20.Listing20_05
         void Fly();
     }
 
-    public class Bird : IFlyable
+    /// <summary>
+    /// 不能在父类中声明Fly()的方法用于子类继承;因为不是所有的鸟都会飞
+    /// </summary>
+    public class Bird
+    {
+    }
+    /// <summary>
+    /// 麻雀
+    /// </summary>
+    public class MaQue :Bird, IFlyable
     {
         public void Fly()
         {
-            Console.WriteLine("鸟会飞");
+            Console.WriteLine("麻雀会飞");
         }
+    }
+    /// <summary>
+    /// 企鹅
+    /// </summary>
+    public class QQ:Bird
+    {
+
     }
 
     public class Plane : IFlyable
@@ -32,7 +48,7 @@ namespace Fowindy.DotNet_Basic_Day20.Listing20_05
             //接口是什么?什么时候使用接口?使用接口的目的是什么?多态
             //鸟会飞,飞机也会飞(因为鸟和飞机没有父类)
             //接口的实现:接口指向子类对象
-            IFlyable fly = new Bird();
+            IFlyable fly = new Plane();//new Bird();
             //表面上调用的是父类的Fly(),实际调用的是子类的Fly()
             fly.Fly();
         }
