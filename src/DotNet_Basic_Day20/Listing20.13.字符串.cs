6308f6c16301aa0b222645b1c047463fdb8599d1
Fowindy <470026339@qq.com>
2019-11-02 19:36:14 +0800

    2.IsNullOrEmpty()函数

--- src/DotNet_Basic_Day20/Listing20.13.字符串.cs
+++ src/DotNet_Basic_Day20/Listing20.13.字符串.cs
@@ -13,6 +13,16 @@ namespace Fowindy.DotNet_Basic_Day20.Listing20_13
             s1 = "李四";//表面上是赋值,实际上在内存中又开辟了一段内存空间;"张三"还在,只是没有变量指向它,成了垃圾数据;最终会被垃圾回收机制GC回收;
             //GC:垃圾回收机制,当程序结束之后,GC会扫描整个内存空间,发现垃圾数据则回收;
             //有些资源GC是不能帮助回收的,比如FileStream(文件流),我们写在Using中
+            //2.IsNullOrEmpty()函数
+            if (string.IsNullOrEmpty(s1))//当s1==""或者s1==null时,为空
+            {
+                Console.WriteLine("为空");
+            }
+            else
+            {
+                Console.WriteLine("不为空");
+            }
+
         }
     }
 }
