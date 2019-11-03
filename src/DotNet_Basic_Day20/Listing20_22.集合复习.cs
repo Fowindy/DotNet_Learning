cc2f7f4207ae1077f7f5e763341dd1f03eb99812
Fowindy <470026339@qq.com>
2019-11-04 02:08:33 +0800

    3.集合:List

--- src/DotNet_Basic_Day20/Listing20_22.集合复习.cs
+++ src/DotNet_Basic_Day20/Listing20_22.集合复习.cs
@@ -25,6 +25,7 @@ namespace Fowindy.DotNet_Basic_Day20.Listing20_22
             #endregion
 
             #region 2.Hashtable
+            Console.WriteLine("----------------------------");
             Hashtable hashtable = new Hashtable();
             hashtable.Add(1, "AAA");
             hashtable.Add(2, "BBB");
@@ -37,6 +38,34 @@ namespace Fowindy.DotNet_Basic_Day20.Listing20_22
             Console.WriteLine(hashtable[1]);//此处输出AAA
             hashtable.Clear();//移除所有元素
             Console.WriteLine(hashtable[1]); //此处将不会有任何输出
+            #endregion
+
+            #region 3.集合:List
+            Console.WriteLine("----------------------------");
+            List<int> list = new List<int>();
+            //添加元素
+            list.Add(123);//123
+            //添加多个元素
+            list.AddRange(new int[] { 456,789,234});//123,456,789,234
+            //在集合序标1的位置插入999
+            list.Insert(1,999);//123,999,456,789,234
+            //在集合序标3的位置插入数组
+            list.InsertRange(3, new int[] { 777, 666, 555 });
+            //123,999,456,777, 666, 555,789,234
+            //移除集合中指定单个元素
+            list.Remove(777);//123,999,456,666, 555,789,234
+            //移除集合序标为5的元素
+            list.RemoveAt(5);//123,999,456,666, 555,234
+            //移除集合序标2开始的两个元素
+            list.RemoveRange(2, 2);//123,999,555,234
+            //判断集合是否包含某个元素
+            if (list.Contains(555))
+            {
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+            }
             #endregion
         }
     }
