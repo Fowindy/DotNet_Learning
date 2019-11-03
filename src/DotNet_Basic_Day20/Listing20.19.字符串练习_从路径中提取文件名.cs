69e4438573118ecb319add6b1aad5ab8985bed45
Fowindy <470026339@qq.com>
2019-11-03 14:41:59 +0800

    练习6.从文件路径中提取文件名(含后缀);比如从c:\a\b.txt中提取b.txt 方法1:字符串的方式实现

---
+++ src/DotNet_Basic_Day20/Listing20.19.字符串练习_从路径中提取文件名.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fowindy.DotNet_Basic_Day20.Listing20_19
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //练习6.从文件路径中提取文件名(含后缀);比如从c:\a\b.txt中提取b.txt
+            string path = @"c:\a\b.txt";
+#if false
+            #region 方法1:字符串的方式实现
+            string[] pathNew = path.Split('\\', StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("文件名为:{0}",pathNew[2]);
+            #endregion
+#elif true
+            #region 方法2:Path.GetFileName()实现
+
+            #endregion
+#endif
+        }
+    }
+}
