51e75276291f0c58d575f5a28a473371b55aa027
Fowindy <470026339@qq.com>
2019-11-03 09:43:13 +0800

    方法二_使用for循环交换实现

--- src/DotNet_Basic_Day20/Listing20.14.字符串练习_翻转字符串.cs
+++ src/DotNet_Basic_Day20/Listing20.14.字符串练习_翻转字符串.cs
@@ -13,8 +13,24 @@ namespace Fowindy.DotNet_Basic_Day20.Listing20_14
             string str = "abcdefg";
             //首先将字符串转换成字符数组
             char[] chs = str.ToCharArray();
+#if false
+            #region 方法1_调用Array.Reverse()方法翻转实现
             //使用Array.Reverse()方法翻转
             Array.Reverse(chs);
+
+            #endregion
+#elif true
+            #region 方法二_使用for循环交换实现
+            for (int i = 0; i < chs.Length/2; i++)
+            {
+                //进行头尾字符交换
+                char temp;
+                temp = chs[i];
+                chs[i] = chs[chs.Length-1-i];
+                chs[chs.Length - 1 - i] = temp;
+            }
+            #endregion
+#endif
             //再将字符数组转换成字符串
             str = new string(chs);
             Console.WriteLine(str);//gfedcba
