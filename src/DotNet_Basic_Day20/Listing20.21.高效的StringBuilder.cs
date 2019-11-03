3909b5385038d5d78f0c6abab9047c411b6d356c
Fowindy <470026339@qq.com>
2019-11-03 22:54:38 +0800

    #1.在字符串序标1的文件插入"ABC"

--- src/DotNet_Basic_Day20/Listing20.21.高效的StringBuilder.cs
+++ src/DotNet_Basic_Day20/Listing20.21.高效的StringBuilder.cs
@@ -13,8 +13,10 @@ namespace Fowindy.DotNet_Basic_Day20.Listing20_21
             //追加元素
             sb.Append("张三");
             sb.Append('#');
+            //插入元素
+            sb.Insert(1, "ABC");//#1.在字符串序标1的文件插入"ABC"
             sb.Append(12.3);
-            Console.WriteLine(sb.ToString());//张三#12.3
+            Console.WriteLine(sb.ToString());//张三#12.3//#1.张ABC三#12.3
         }
     }
 }
