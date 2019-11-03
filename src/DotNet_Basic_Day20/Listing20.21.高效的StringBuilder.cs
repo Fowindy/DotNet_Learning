b7fb6160955125fb51000bd4bd8b8988cc8b5126
Fowindy <470026339@qq.com>
2019-11-03 22:57:20 +0800

    #2.将字符串中'#'替换成'Z'

--- src/DotNet_Basic_Day20/Listing20.21.高效的StringBuilder.cs
+++ src/DotNet_Basic_Day20/Listing20.21.高效的StringBuilder.cs
@@ -15,8 +15,10 @@ namespace Fowindy.DotNet_Basic_Day20.Listing20_21
             sb.Append('#');
             //插入元素
             sb.Insert(1, "ABC");//#1.在字符串序标1的文件插入"ABC"
+            //替换元素
+            sb.Replace('#', 'Z');//#2.将字符串中'#'替换成'Z'
             sb.Append(12.3);
-            Console.WriteLine(sb.ToString());//张三#12.3//#1.张ABC三#12.3
+            Console.WriteLine(sb.ToString());//张三#12.3//#1.张ABC三#12.3//#2.张ABC三Z12.3
         }
     }
 }
