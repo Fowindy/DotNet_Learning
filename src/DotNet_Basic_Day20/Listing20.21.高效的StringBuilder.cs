6d594ecaa376cc175c22b9339e4c9ce91123f822
Fowindy <470026339@qq.com>
2019-11-03 22:52:13 +0800

    StringBuilder类Append追加元素方法

--- src/DotNet_Basic_Day20/Listing20.21.高效的StringBuilder.cs
+++ src/DotNet_Basic_Day20/Listing20.21.高效的StringBuilder.cs
@@ -8,6 +8,13 @@ namespace Fowindy.DotNet_Basic_Day20.Listing20_21
     {
         public static void Main()
         {
+            //实例化
+            StringBuilder sb = new StringBuilder();
+            //追加元素
+            sb.Append("张三");
+            sb.Append('#');
+            sb.Append(12.3);
+            Console.WriteLine(sb.ToString());//张三#12.3
         }
     }
 }
