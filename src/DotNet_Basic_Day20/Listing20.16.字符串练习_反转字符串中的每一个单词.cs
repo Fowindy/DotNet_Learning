75386882ff3fb89e0b771dc0ec5c7c6e43236ee2
Fowindy <470026339@qq.com>
2019-11-03 14:16:25 +0800

    课上练习3：接收用户输入的一句英文，将其中的单词以反序输出.“I Love You"→“I evoL uoY"

---
+++ src/DotNet_Basic_Day20/Listing20.16.字符串练习_反转字符串中的每一个单词.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fowindy.DotNet_Basic_Day20.Listing20_16
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //课上练习3：接收用户输入的一句英文，将其中的单词以反序输出.“I Love You"→“I evoL uoY"
+            string str = "I Love You";
+            //首先拿到每一个单词
+            string[] strNew = str.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            //遍历每一个单词
+            for (int i = 0; i < strNew.Length; i++)
+            {
+                //将单词转换成字符数组
+                char[] chs = strNew[i].ToCharArray();
+                //对字符数组进行反转交换
+                for (int j = 0; j < strNew[i].Length / 2; j++)
+                {
+                    char temp;
+                    temp = chs[j];
+                    chs[j] = chs[strNew[i].Length - 1 - j];
+                    chs[strNew[i].Length - 1 - j] = temp;
+                }
+                //将交换后的字符数组转换成字符串存储到字符串数组中
+                strNew[i] = new string(chs);
+            }
+            //将字符串数组加入空格连接成字符串
+            string str1 = string.Join(' ', strNew);
+            //输出结果
+            Console.WriteLine(str1);//I evoL uoY
+        }
+    }
+}
