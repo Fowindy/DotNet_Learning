2cf9b48a15b360bde582322217d15166579f2a4f
Fowindy <470026339@qq.com>
2019-11-07 00:04:21 +0800

    #2.File类相关函数

---
+++ src/DotNet_Basic_Day21/Listing21_02.File类.cs
@@ -0,0 +1,17 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2019年11月7日 星期四 00:03:37
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fowindy.DotNet_Basic_Day21.Listing21_02
+{
+    public class Listing21_02
+    {
+        public static void Main()
+        {
+        }
+    }
+}
