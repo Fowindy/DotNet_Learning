282b87b89d1c6cc9da5da90540f1d115a0d4810a
Fowindy <470026339@qq.com>
2019-11-06 12:59:11 +0800

    #13.静态和非静态

---
+++ src/DotNet_Basic_Day20/Listing20_25.静态和非静态的区别.cs
@@ -0,0 +1,17 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2019年11月6日 星期三 12:57:15
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fowindy.DotNet_Basic_Day20.Listing20_25
+{
+    public class Listing20_25
+    {
+        public static void Main()
+        {
+        }
+    }
+}
