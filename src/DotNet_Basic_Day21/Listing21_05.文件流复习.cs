22aadbf4bac5b09aaf93597866dfda5c2bdbde81
Fowindy <470026339@qq.com>
2019-11-07 16:46:33 +0800

    #5.文件流练习

---
+++ src/DotNet_Basic_Day21/Listing21_05.文件流复习.cs
@@ -0,0 +1,17 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2019年11月7日 星期四 16:43:03
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fowindy.DotNet_Basic_Day21.Listing21_05
+{
+    public class Listing21_05
+    {
+        public static void Main()
+        {
+        }
+    }
+}
