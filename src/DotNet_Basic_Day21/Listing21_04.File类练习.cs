dcb9767ac915f446781c81572b0dd17a4424625f
Fowindy <470026339@qq.com>
2019-11-07 16:41:24 +0800

    #4.File类练习

---
+++ src/DotNet_Basic_Day21/Listing21_04.File类练习.cs
@@ -0,0 +1,19 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2019年11月7日 星期四 16:18:54
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Fowindy.DotNet_Basic_Day21.Listing21_04
+{
+    public class Listing21_04
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
