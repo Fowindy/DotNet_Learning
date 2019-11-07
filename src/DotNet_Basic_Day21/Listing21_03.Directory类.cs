76fe003011bd4368690369ea05208e26f77d9423
Fowindy <470026339@qq.com>
2019-11-07 16:04:27 +0800

    $3.4.1GetFiles:获取目录中的所有文件的全路径

--- src/DotNet_Basic_Day21/Listing21_03.Directory类.cs
+++ src/DotNet_Basic_Day21/Listing21_03.Directory类.cs
@@ -14,18 +14,30 @@ namespace Fowindy.DotNet_Basic_Day21.Listing21_03
     {
         public static void Main()
         {
+#if true
             #region $3.1.CreateDirectory:创建文件夹
             Directory.CreateDirectory(@"C:\Users\Administrator\Desktop\文件夹");
-            File.Create(@"C:\Users\Administrator\Desktop\文件夹\new.txt");
-            Console.WriteLine("OK");
-            #endregion
-            
+            using (File.Create(@"C:\Users\Administrator\Desktop\文件夹\new.txt"))
+            {
+                Console.WriteLine("OK");
+            }
+            #endregion  
+#endif
+
             #region $3.3.Move:移动文件夹
             Directory.Move(@"C:\Users\Administrator\Desktop\文件夹", @"C:\Users\Administrator\Desktop\文件夹2");
             #endregion
 
             #region $3.2.2.Delete:删除文件夹(目录不为空不异常,永久删除)
             Directory.Delete(@"C:\Users\Administrator\Desktop\文件夹2", true);
+            #endregion
+
+            #region $3.4.1.GetFiles:获取目录中的所有文件的全路径
+            string[] path = Directory.GetFiles(@"J:\Documents\学习笔记\Music");
+            for (int i = 0; i < path.Length; i++)
+            {
+                Console.WriteLine(path[i]);
+            }
             #endregion
         }
     }
