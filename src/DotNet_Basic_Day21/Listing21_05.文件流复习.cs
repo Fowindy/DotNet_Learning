fd467cb46c82591da1db1a1666498c089875b107
Fowindy <470026339@qq.com>
2019-11-07 21:17:41 +0800

    @5.1.1.文件流读取文件

--- src/DotNet_Basic_Day21/Listing21_05.文件流复习.cs
+++ src/DotNet_Basic_Day21/Listing21_05.文件流复习.cs
@@ -18,16 +18,18 @@ namespace Fowindy.DotNet_Basic_Day21.Listing21_05
             //StreamReader/StreamWriter:操作字符
 
             //using自动释放文件流资源
+            #region @5.1.1.文件流读取文件
             using (FileStream fsRead = new FileStream(@"J:\Documents\学习笔记\文档\应用程序临时文件路径更改.txt", FileMode.OpenOrCreate, FileAccess.Read))
             {
                 //建立单次最大读取的字节缓存区,为避免崩溃,每次最大读取5M
-                byte[] buffer = new byte[1024*1024*5];
+                byte[] buffer = new byte[1024 * 1024 * 5];
                 //实际读取到的字节数
                 int r = fsRead.Read(buffer, 0, buffer.Length);
                 //将字节解码成字符串
                 string str = Encoding.Default.GetString(buffer, 0, r);
                 Console.WriteLine(str);
-            }
+            } 
+            #endregion
             #endregion
         }
     }
