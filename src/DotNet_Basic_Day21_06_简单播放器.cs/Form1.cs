da090f7e2f949151f94a1885773f7b3edce308e3
Fowindy <470026339@qq.com>
2019-11-08 09:04:27 +0800

    #7.WinForm_简单播放器

---
+++ src/DotNet_Basic_Day21_06_简单播放器.cs/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNet_Basic_Day21_06_简单播放器.cs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MusicPlayer.Ctlcontrols.play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MusicPlayer.Ctlcontrols.pause();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MusicPlayer.Ctlcontrols.stop();
+        }
+    }
+}
