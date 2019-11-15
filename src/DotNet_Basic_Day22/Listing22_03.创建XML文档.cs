2f03ebd79814c42901dfdbb8e17b5fad80ffdcdf
Fowindy <470026339@qq.com>
2019-11-15 22:28:11 +0800

    $3.1.4.创建根节点

--- src/DotNet_Basic_Day22/Listing22_03.创建XML文档.cs
+++ src/DotNet_Basic_Day22/Listing22_03.创建XML文档.cs
@@ -20,6 +20,8 @@ namespace Fowindy.DotNet_Basic_Day22.Listing22_03
             XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", "utf-8", null);
             //$3.1.3.把声明行添加到doc中
             doc.AppendChild(dec);
+            //$3.1.4.创建根节点
+            XmlElement Books = doc.CreateElement("Books");
         }
     }
 }
