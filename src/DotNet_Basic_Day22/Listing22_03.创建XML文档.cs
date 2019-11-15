85adf2ecb7b6bab2fdefe7a72311e8e7bcdbdb13
Fowindy <470026339@qq.com>
2019-11-15 14:16:03 +0800

    $3.1.2.加入声明行

--- src/DotNet_Basic_Day22/Listing22_03.创建XML文档.cs
+++ src/DotNet_Basic_Day22/Listing22_03.创建XML文档.cs
@@ -16,6 +16,8 @@ namespace Fowindy.DotNet_Basic_Day22.Listing22_03
             //$3.1.代码创建xlm文档
             //$3.1.1.创建xml实例对象
             XmlDocument doc = new XmlDocument();
+            //$3.1.2.加入声明行
+            XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", "utf-8", null);
         }
     }
 }
