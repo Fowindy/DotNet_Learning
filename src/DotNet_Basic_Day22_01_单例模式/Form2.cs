c651a2f772c84621b93fd3ee49b4980779bb374d
Fowindy <470026339@qq.com>
2019-11-15 11:44:02 +0800

    $1.3.单例模式实现完成

--- src/DotNet_Basic_Day22_01_单例模式/Form2.cs
+++ src/DotNet_Basic_Day22_01_单例模式/Form2.cs
@@ -12,6 +12,8 @@ namespace DotNet_Basic_Day22_01_单例模式
 {
     public partial class Form2 : Form
     {
+        //创建全局唯一的单例
+        public static Form2 FrmSingle = null;
         //因为from2的构造函数为public,所以没办法单例;需要将form2构造函数私有化
         private Form2()
         {
@@ -22,8 +24,17 @@ namespace DotNet_Basic_Day22_01_单例模式
         public static Form2 GetSingle()
         {
             //在自己的静态方法中实例化form2对象
-            Form2 form2 = new Form2();
-            return form2;
+            //判断是否为null,为null则创建,反之则不创建
+            //$1.3.单例模式实现完成
+            if (FrmSingle == null)
+            {
+                FrmSingle = new Form2();
+            }
+            else
+            {
+                MessageBox.Show("已经打开了一个应用");
+            }
+            return FrmSingle;
         }
     }
 }
