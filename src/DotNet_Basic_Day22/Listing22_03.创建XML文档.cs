c5fd140b4fa7f2317dd86e15102dfbf27a7dd1c6
Fowindy <470026339@qq.com>
2019-11-15 12:25:50 +0800

    #03.XML

---
+++ src/DotNet_Basic_Day22/Listing22_03.创建XML文档.cs
@@ -0,0 +1,17 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2019年11月15日 星期五 11:51:18
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fowindy.DotNet_Basic_Day22.Listing22_03
+{
+    public class Listing22_03
+    {
+        public static void Main()
+        {
+        }
+    }
+}
