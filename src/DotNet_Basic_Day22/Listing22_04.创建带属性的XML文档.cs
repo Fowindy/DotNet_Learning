b5ee9563e9ba7754a587074e82cdec5eb650f4f7
Fowindy <470026339@qq.com>
2019-11-16 09:25:39 +0800

    #4.创建带属性的XML文档

---
+++ src/DotNet_Basic_Day22/Listing22_04.创建带属性的XML文档.cs
@@ -0,0 +1,19 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2019年11月16日 星期六 09:20:38
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Fowindy.DotNet_Basic_Day22.Listing22_04
+{
+    public class Listing22_04
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
