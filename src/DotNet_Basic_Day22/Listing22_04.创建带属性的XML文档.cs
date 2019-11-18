4c0dbf9f78a4ea8c489d71a7cfdc628d565bf96f
Fowindy <470026339@qq.com>
2019-11-18 09:49:55 +0800

    $4.1.xml主体框架完成

--- src/DotNet_Basic_Day22/Listing22_04.创建带属性的XML文档.cs
+++ src/DotNet_Basic_Day22/Listing22_04.创建带属性的XML文档.cs
@@ -13,7 +13,25 @@ namespace Fowindy.DotNet_Basic_Day22.Listing22_04
     {
         public static void Main()
         {
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", "utf-8", null);
+            doc.AppendChild(dec);
 
+            XmlElement Order = doc.CreateElement("Order");
+            doc.AppendChild(Order);
+
+            XmlElement CustomerName = doc.CreateElement("CustomerName");
+            CustomerName.InnerText = "刘洋";
+            Order.AppendChild(CustomerName);
+
+            XmlElement OrderNumber = doc.CreateElement("OrderNumber");
+            OrderNumber.InnerText = "bj0000001";
+            Order.AppendChild(OrderNumber);
+
+            XmlElement Items = doc.CreateElement("Items");
+
+            Order.AppendChild(Items);
+            doc.Save("Order.xml");
         }
     }
 }
