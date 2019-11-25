b536745114a65a7e333c5292611d04c89e02817a
Fowindy <470026339@qq.com>
2019-11-25 20:10:08 +0800

    @5.2.1.创建描述信息

--- src/DotNet_Basic_Day22/Listing22_05.Xml文件追加内容.cs
+++ src/DotNet_Basic_Day22/Listing22_05.Xml文件追加内容.cs
@@ -16,36 +16,43 @@ namespace Fowindy.DotNet_Basic_Day22.Listing22_05
         {
             //创建xml文档实例对象
             XmlDocument doc = new XmlDocument();
+            XmlElement books;
             //#5.1.如果文件已经存在
             if (File.Exists("Books.xml"))
             {
                 //@5.1.1.加载该xml文件到实例对象
                 doc.Load("Books.xml");
                 //@5.1.2.获取该xml文件的根节点
-                XmlElement books = doc.DocumentElement;
-                //@5.1.3.在该根节点上添加子节点
-                XmlElement book3 = doc.CreateElement("book3");
-                books.AppendChild(book3);
+                books = doc.DocumentElement;
 
-                XmlElement name3 = doc.CreateElement("Name3");
-                name3.InnerText = "三国演义";
-                book3.AppendChild(name3);
-
-                XmlElement price3 = doc.CreateElement("Price3");
-                price3.InnerText = "198";
-                book3.AppendChild(price3);
-
-                XmlElement des3 = doc.CreateElement("Des3");
-                des3.InnerText = "乱世出英雄";
-                book3.AppendChild(des3);
-
-                doc.Save("Books.xml");
             }
             //#5.2.如果文件不存在,则创建xml文件
             else
             {
-
+                //@5.2.2.创建描述信息
+                XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", "utf-8", null);
+                doc.AppendChild(dec);
+                //@5.2.1.创建根节点
+                books = doc.CreateElement("Books");
+                doc.AppendChild(books);
             }
+            //@5.1.3.在该根节点上添加子节点
+            XmlElement book3 = doc.CreateElement("book3");
+            books.AppendChild(book3);
+
+            XmlElement name3 = doc.CreateElement("Name3");
+            name3.InnerText = "三国演义";
+            book3.AppendChild(name3);
+
+            XmlElement price3 = doc.CreateElement("Price3");
+            price3.InnerText = "198";
+            book3.AppendChild(price3);
+
+            XmlElement des3 = doc.CreateElement("Des3");
+            des3.InnerText = "乱世出英雄";
+            book3.AppendChild(des3);
+
+            doc.Save("Books.xml");
         }
     }
 }
