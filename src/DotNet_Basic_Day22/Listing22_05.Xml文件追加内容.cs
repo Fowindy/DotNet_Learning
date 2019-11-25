9d86533910fe23eab9ada355fdfa88bb39483950
Fowindy <470026339@qq.com>
2019-11-25 18:17:08 +0800

    $5.Xml文件追加内容

---
+++ src/DotNet_Basic_Day22/Listing22_05.Xml文件追加内容.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2019年11月25日 星期一 17:33:45
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Fowindy.DotNet_Basic_Day22.Listing22_05
+{
+    public class Listing22_05
+    {
+        public static void Main()
+        {
+        }
+    }
+}
