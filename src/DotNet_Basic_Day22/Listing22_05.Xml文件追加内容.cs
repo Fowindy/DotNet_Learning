5225f79cb8e0fa6291d396e34c26f40e22c67d42
Fowindy <470026339@qq.com>
2019-11-25 19:16:49 +0800

    #5.1.如果文件已经存在

--- src/DotNet_Basic_Day22/Listing22_05.Xml文件追加内容.cs
+++ src/DotNet_Basic_Day22/Listing22_05.Xml文件追加内容.cs
@@ -4,6 +4,7 @@
  * ==============================================================================*/
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Xml;
 
@@ -13,6 +14,12 @@ namespace Fowindy.DotNet_Basic_Day22.Listing22_05
     {
         public static void Main()
         {
+            //创建xml文档实例对象
+            XmlDocument doc = new XmlDocument();
+            //#5.1.如果文件已经存在
+            if (File.Exists("Books.xml"))
+            {
+            }
         }
     }
 }
