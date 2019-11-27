d1866f4f009b1f36db2194b152cbab95d8216dd0
Fowindy <fowindy@outlook.com>
2019-11-27 13:09:48 +0800

    $7.读取带属性的xml文档

---
+++ src/DotNet_Basic_Day22/Listing22_07.读取带属性的xml文档.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2019年11月27日 星期三 13:09:18
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Fowindy.DotNet_Basic_Day22.Listing22_07
+{
+    public class Listing22_07
+    {
+        public static void Main()
+        {
+        }
+    }
+}
