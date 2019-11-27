4e181c4c95e14e67bd8642e419f114542752a0b8
Fowindy <fowindy@outlook.com>
2019-11-27 11:07:01 +0800

    $6.读取xml文档

---
+++ src/DotNet_Basic_Day22/Listing22_06.读取xml文档.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2019年11月27日 星期三 11:05:39
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Fowindy.DotNet_Basic_Day22.Listing22_06
+{
+    public class Listing22_06
+    {
+        public static void Main()
+        {
+        }
+    }
+}
