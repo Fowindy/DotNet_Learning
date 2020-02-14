3b67f4d7d45bcfdcdf19b01b3954e424102394dc
Fowindy <470026339@qq.com>
2020-02-14 23:14:54 +0800

    #17.1.声明一个无参无返回值的委托

--- src/DotNet_Basic_Day22/Listing22_17.多播委托.cs
+++ src/DotNet_Basic_Day22/Listing22_17.多播委托.cs
@@ -9,6 +9,8 @@ using System.Xml;
 
 namespace Fowindy.DotNet_Basic_Day22.Listing22_17
 {
+    ///<![CDATA[1.声明一个无参无返回值的委托]]>
+    public delegate void DelTest();
     public class Listing22_17
     {
         public static void Main()
