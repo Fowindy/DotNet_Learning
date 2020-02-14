efe96362631b84d561da2329dfea6cb64ee6ad29
Fowindy <470026339@qq.com>
2020-02-14 10:44:39 +0800

    #13.2.合二为一可行,委托方法构建完成

--- src/DotNet_Basic_Day22/Listing22_13.委托应用_求数组的最大值.cs
+++ src/DotNet_Basic_Day22/Listing22_13.委托应用_求数组的最大值.cs
@@ -9,6 +9,13 @@ using System.Xml;
 
 namespace Fowindy.DotNet_Basic_Day22.Listing22_13
 {
+    /// <summary>
+    /// 创建委托类型
+    /// </summary>
+    /// <param name="object1">比较的第一项</param>
+    /// <param name="object2">比较的第二项</param>
+    /// <returns></returns>
+    public delegate int DelCompara(object object1, object object2);
     public class Listing22_13
     {
         public static void Main()
@@ -26,12 +33,17 @@ namespace Fowindy.DotNet_Basic_Day22.Listing22_13
         /// </summary>
         /// <param name="nums">整型数组</param>
         /// <returns></returns>
-        public static int GetMax(int[] nums)
+        ///<![CDATA[传入委托方法参数]]>
+        public static Object GetMax(Object[] nums,DelCompara delCompara)
         {
-            int max = nums[0];
+            Object max = nums[0];
             for (int i = 0; i < nums.Length; i++)
             {
-                if (max < nums[i])
+                ///<![CDATA[分析:if语句中需要传一个比较的方法委托,来区分是何种比较
+                ///         if里面为bool类型,将比较转换成max-num[i] < 0
+                ///         则max-num[i]为整型,也就是委托的返回值为整型]]>
+                ///<![CDATA[调用委托方法]]>
+                if (delCompara(max,nums[i]) < 0)
                 {
                     max = nums[i];
                 }
@@ -43,17 +55,17 @@ namespace Fowindy.DotNet_Basic_Day22.Listing22_13
         /// </summary>
         /// <param name="names">字符数组</param>
         /// <returns></returns>
-        public static string GetMax(string[] names)
-        {
-            string max = names[0];
-            for (int i = 0; i < names.Length; i++)
-            {
-                if (max.Length < names[i].Length)
-                {
-                    max = names[i];
-                }
-            }
-            return max;
-        }
+        //public static string GetMax(string[] names)
+        //{
+        //    string max = names[0];
+        //    for (int i = 0; i < names.Length; i++)
+        //    {
+        //        if (max.Length < names[i].Length)
+        //        {
+        //            max = names[i];
+        //        }
+        //    }
+        //    return max;
+        //}
     }
 }
