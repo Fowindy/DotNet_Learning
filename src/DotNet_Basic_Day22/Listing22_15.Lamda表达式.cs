fbd4ed803981fd5795bc4956b1f4bb7c73365e52
Fowindy <470026339@qq.com>
2020-02-14 17:18:09 +0800

    $15.Lamda表达式

---
+++ src/DotNet_Basic_Day22/Listing22_15.Lamda表达式.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月14日 星期五 17:12:03
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Fowindy.DotNet_Basic_Day22.Listing22_15
+{
+    public class Listing22_15
+    {
+        public static void Main()
+        {
+        }
+    }
+}
