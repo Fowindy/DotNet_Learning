99eb729bacb39cb297d98140f006cc52f4a7d26c
Fowindy <470026339@qq.com>
2020-02-14 15:40:51 +0800

    #14.1.问题的提出1:如13节涉及到强制转换object,就是容易出现拆装箱

--- src/DotNet_Basic_Day22/Listing22_14.泛型委托.cs
+++ src/DotNet_Basic_Day22/Listing22_14.泛型委托.cs
@@ -11,6 +11,8 @@ namespace Fowindy.DotNet_Basic_Day22.Listing22_14
 {
     public class Listing22_14
     {
+        ///<![CDATA[问题的提出1:如13节涉及到强制转换object,就是容易出现拆装箱
+        ///         解决的方案1:采用泛型委托来解决拆装箱的问题]]>
         public static void Main()
         {
         }
