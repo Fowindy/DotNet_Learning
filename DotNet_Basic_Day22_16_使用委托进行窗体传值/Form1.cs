9e4c504bd3c1c0f7731ab4999301b296269e73c3
Fowindy <470026339@qq.com>
2020-02-14 20:04:54 +0800

    $16.DotNet_Basic_Day22_16_使用委托进行窗体传值

---
+++ DotNet_Basic_Day22_16_使用委托进行窗体传值/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNet_Basic_Day22_16_使用委托进行窗体传值
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
