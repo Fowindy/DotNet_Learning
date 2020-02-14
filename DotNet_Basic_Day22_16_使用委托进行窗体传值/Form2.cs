30acdbe9dd6de064fe6f3a69665d6f882e35ed5a
Fowindy <470026339@qq.com>
2020-02-14 22:09:22 +0800

    #16.4.窗体2中声明一个委托

--- DotNet_Basic_Day22_16_使用委托进行窗体传值/Form2.cs
+++ DotNet_Basic_Day22_16_使用委托进行窗体传值/Form2.cs
@@ -10,11 +10,18 @@ using System.Windows.Forms;
 
 namespace DotNet_Basic_Day22_16_使用委托进行窗体传值
 {
+    ///<![CDATA[4.窗体2中声明一个委托]]>
+    //委托必须和窗体1中的方法有相同的签名
+    public delegate void DelShowMsg(string str);
     public partial class Form2 : Form
     {
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
         }
     }
 }
