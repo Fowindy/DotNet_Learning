85cf4f274326de269812038a5c55612656358cbc
Fowindy <470026339@qq.com>
2020-02-14 16:39:47 +0800

    #14.2.强转object会发生拆装箱

--- src/DotNet_Basic_Day22/Listing22_14.泛型委托.cs
+++ src/DotNet_Basic_Day22/Listing22_14.泛型委托.cs
@@ -9,12 +9,26 @@ using System.Xml;
 
 namespace Fowindy.DotNet_Basic_Day22.Listing22_14
 {
+    public delegate int DelCompare(object obj1, object obj2);
     public class Listing22_14
     {
         ///<![CDATA[问题的提出1:如13节涉及到强制转换object,就是容易出现拆装箱
         ///         解决的方案1:采用泛型委托来解决拆装箱的问题]]>
         public static void Main()
         {
+        }
+
+        public static object GetMax(object[] nums,DelCompare delCompare)
+        {
+            object max = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (delCompare(max,nums[i]) < 0)
+                {
+                    max = nums[i];
+                }
+            }
+            return max;
         }
     }
 }
