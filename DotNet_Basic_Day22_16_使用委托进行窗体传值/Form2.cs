4903279ae7f5e1badb6c655dd087a270955fb8ab
Fowindy <470026339@qq.com>
2020-02-14 22:17:56 +0800

    #16.5.问题2:怎样将方法从Form1传到Form2中+分析

--- DotNet_Basic_Day22_16_使用委托进行窗体传值/Form2.cs
+++ DotNet_Basic_Day22_16_使用委托进行窗体传值/Form2.cs
@@ -23,5 +23,10 @@ namespace DotNet_Basic_Day22_16_使用委托进行窗体传值
         private void button1_Click(object sender, EventArgs e)
         {
         }
+
+        ///<![CDATA[5.问题2:怎样将方法从Form1传到Form2中
+        ///         分析:因为在Form1的按钮事件中创建了Form2的对象,
+        ///         创建窗体2对象的时候调用了构造函数,通过构造函数
+        ///         将方法从Form1传到Form2中]]>
     }
 }
