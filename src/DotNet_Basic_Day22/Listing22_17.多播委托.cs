7890c6c360072db6569071435d6c2dbace12aca4
Fowindy <470026339@qq.com>
2020-02-14 23:06:10 +0800

    $17.多播委托

---
+++ src/DotNet_Basic_Day22/Listing22_17.多播委托.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月14日 星期五 23:05:39
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Fowindy.DotNet_Basic_Day22.Listing22_17
+{
+    public class Listing22_17
+    {
+        public static void Main()
+        {
+        }
+    }
+}
