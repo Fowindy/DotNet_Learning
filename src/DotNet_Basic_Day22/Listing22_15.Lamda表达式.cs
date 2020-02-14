38550fec67113277bf4aedb3dcf7dd112f22812f
Fowindy <470026339@qq.com>
2020-02-14 17:21:39 +0800

    #15.1.声明三种类型的委托

--- src/DotNet_Basic_Day22/Listing22_15.Lamda表达式.cs
+++ src/DotNet_Basic_Day22/Listing22_15.Lamda表达式.cs
@@ -9,6 +9,15 @@ using System.Xml;
 
 namespace Fowindy.DotNet_Basic_Day22.Listing22_15
 {
+    /// <summary>
+    /// 1.声明三种类型的委托
+    /// </summary>
+    ///<![CDATA[第一种:无参数无返回值]]>
+    public delegate void DelOne();
+    ///<![CDATA[第二种:有参数无返回值]]>
+    public delegate void DelTwo(string name);
+    ///<![CDATA[第三种:有参数有返回值]]>
+    public delegate string DelThree(string name);
     public class Listing22_15
     {
         public static void Main()
