976b16a5076e7f6919d4da2a04e171c6c4ad2d31
Fowindy <470026339@qq.com>
2020-02-14 15:37:16 +0800

    $14.泛型委托

---
+++ src/DotNet_Basic_Day22/Listing22_14.泛型委托.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月14日 星期五 15:34:24
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Fowindy.DotNet_Basic_Day22.Listing22_14
+{
+    public class Listing22_14
+    {
+        public static void Main()
+        {
+        }
+    }
+}
