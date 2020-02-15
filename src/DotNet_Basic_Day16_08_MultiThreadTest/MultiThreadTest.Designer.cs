2a04bf7a2258b5e790fc7fa85abde1f670f86121
Fowindy <470026339@qq.com>
2020-02-15 17:43:59 +0800

    DotNet_Basic_Day16_08_MultiThreadTest_蜂鸣器多线程测试

---
+++ src/DotNet_Basic_Day16_08_MultiThreadTest/MultiThreadTest.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MultiThreadTest
+{
+    partial class MultiThreadTest
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textLog
+            // 
+            this.textLog.Location = new System.Drawing.Point(81, 41);
+            this.textLog.Multiline = true;
+            this.textLog.Name = "textLog";
+            this.textLog.Size = new System.Drawing.Size(368, 346);
+            this.textLog.TabIndex = 0;
+            // 
+            // MultiThreadTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 528);
+            this.Controls.Add(this.textLog);
+            this.Name = "MultiThreadTest";
+            this.Text = "MultiThreadTest";
+            this.Load += new System.EventHandler(this.MultiThreadTest_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textLog;
+    }
+}
+
---
+++ src/DotNet_Basic_Day16_08_MultiThreadTest/MultiThreadTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiThreadTest
+{
+    public partial class MultiThreadTest : Form
+    {
+        public MultiThreadTest()
+        {
+            InitializeComponent();
+
+        }
+        bool IsStart = false;
+        private void ShowMsg(object obj)
+        {
+            while (!IsStart)
+            {
+                textLog.AppendText("蜂鸣器鸣叫" + "\r\n");
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        private void ShowMsgBox(object obj)
+        {
+            if (MessageBox.Show("阻挡动点未感应", "报警", MessageBoxButtons.OK) == DialogResult.OK)
+            {
+                IsStart = true;
+            }
+        }
+
+        private void MultiThreadTest_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread th1 = new Thread(ShowMsg);
+            th1.IsBackground = true;
+            th1.Start();
+
+            Thread th2 = new Thread(ShowMsgBox);
+            th2.IsBackground = true;
+            th2.Start();
+        }
+    }
+}
