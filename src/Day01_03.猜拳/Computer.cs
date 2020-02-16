a32285ef762e5863c41ab17b35f807d20df0e4b6
Fowindy <470026339@qq.com>
2020-02-16 17:48:47 +0800

    @03.02.电脑出拳的方法并将随机整数转换为出拳内容

--- src/Day01_03.猜拳/Computer.cs
+++ src/Day01_03.猜拳/Computer.cs
@@ -14,6 +14,23 @@ namespace Day01_03.猜拳
         /// <summary>
         /// 03.01.定义记录电脑出拳内容的属性
         /// </summary>
-        public string ComputerResult { get; set; }
+        public string FirstName { get; set; }
+        /// <summary>
+        /// 03.02.电脑出拳的方法并将随机整数转换为出拳内容
+        /// </summary>
+        /// <returns></returns>
+        ///<![CDATA[注意:电脑出拳的内容是随机数,不需要转换,因此无参数]]>
+        public int Play()
+        {
+            int num = new Random().Next(1, 4);
+            //将随机整数转换为出拳内容
+            switch (num)
+            {
+                case 1: FirstName = "石头";break;
+                case 2: FirstName = "剪刀";break;
+                case 3:FirstName = "布";break;
+            }
+            return num;
+        }
     }
 }
