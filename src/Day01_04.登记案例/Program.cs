5071332823c659871cd4ef7cb2c81f184e8beb0c
Fowindy <470026339@qq.com>
2020-02-16 22:34:13 +0800

    $Day01_04.登记案例

---
+++ src/Day01_04.登记案例/Program.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月16日 星期日 22:33:38
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Day01_04.登记案例
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
