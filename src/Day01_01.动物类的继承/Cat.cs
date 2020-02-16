c5268dd723f02e365c869517b7d8d958d670421e
Fowindy <470026339@qq.com>
2020-02-16 10:04:10 +0800

    #01.03.Cat.cs子类完成

---
+++ src/Day01_01.动物类的继承/Cat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day01_01.动物类的继承
+{
+    //Cat子类继承Animal父类
+    public class Cat : Animal
+    {
+        //子类必须实现(重写)父类中的抽象方法,
+        //除非子类也是抽象类
+        public override void Eat()
+        {
+            Console.WriteLine("猫咪舔着吃...");
+        }
+
+        public override void Shout()
+        {
+            Console.WriteLine("猫咪喵喵叫...");
+        }
+    }
+}
