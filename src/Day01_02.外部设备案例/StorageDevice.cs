61e33c6dbe9776d18968339d87ea3014e8fe11b4
Fowindy <470026339@qq.com>
2020-02-16 14:06:02 +0800

    #02.02.移动存储设备父类_父类需抽象给子类继承

--- src/Day01_02.外部设备案例/StorageDevice.cs
+++ src/Day01_02.外部设备案例/StorageDevice.cs
@@ -9,7 +9,18 @@ using System.Xml;
 
 namespace Day01_02.外部设备案例
 {
-    public class StorageDevice
+    /// <summary>
+    /// 移动存储设备父类_父类需抽象给子类继承
+    /// </summary>
+    public abstract class StorageDevice
     {
+        /// <summary>
+        /// 移动存储设备的读取
+        /// </summary>
+        public abstract void Read();
+        /// <summary>
+        /// 移动存储设备的写入
+        /// </summary>
+        public abstract void Write();
     }
 }
