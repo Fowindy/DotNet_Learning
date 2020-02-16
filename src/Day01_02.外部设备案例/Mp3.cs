f3fd5b089f5ea0d66495cb84ba96cd55baeff509
Fowindy <470026339@qq.com>
2020-02-16 14:12:38 +0800

    #02.03.Mp3子类继承StorageDevice并重写读写方法

---
+++ src/Day01_02.外部设备案例/Mp3.cs
@@ -0,0 +1,27 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月16日 星期日 14:08:58
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Day01_02.外部设备案例
+{
+    /// <summary>
+    /// 03.Mp3子类继承StorageDevice并重写读写方法
+    /// </summary>
+    public class Mp3:StorageDevice
+    {
+        public override void Read()
+        {
+            Console.WriteLine("Mp3读取中...");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("Mp3写入中...");
+        }
+    }
+}
