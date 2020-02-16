d50e87a4600a040d215f692d2b8484973b4353de
Fowindy <470026339@qq.com>
2020-02-16 23:20:56 +0800

    #04.Student类

---
+++ src/Day01_04.登记案例/Student.cs
@@ -0,0 +1,15 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月16日 星期日 23:19:57
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Day01_04.登记案例
+{
+    public class Student
+    {
+    }
+}
