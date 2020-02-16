7240bab802b2b986e42e77c543b63a584f3ba595
Fowindy <470026339@qq.com>
2020-02-16 10:05:44 +0800

    #01.04.Dog.cs子类完成

---
+++ src/Day01_01.动物类的继承/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day01_01.动物类的继承
+{
+    //Dog子类继承Animal父类
+    public class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("狗狗咬着吃...");
+        }
+
+        public override void Shout()
+        {
+            Console.WriteLine("狗狗汪汪叫...");
+        }
+    }
+}
