9cf6b944afea65dd9136a4450b562da73b9dffcc
Fowindy <470026339@qq.com>
2020-02-16 16:18:08 +0800

    #01.界面设计完成

--- src/Day01_03.猜拳/Form1.Designer.cs
+++ src/Day01_03.猜拳/Form1.Designer.cs
@@ -28,13 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPlayerResult = new System.Windows.Forms.Label();
+            this.lblComputerResult = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnStone = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.btnCloth = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "玩家";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(314, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "电脑";
+            // 
+            // lblPlayerResult
+            // 
+            this.lblPlayerResult.AutoSize = true;
+            this.lblPlayerResult.Location = new System.Drawing.Point(142, 58);
+            this.lblPlayerResult.Name = "lblPlayerResult";
+            this.lblPlayerResult.Size = new System.Drawing.Size(41, 12);
+            this.lblPlayerResult.TabIndex = 2;
+            this.lblPlayerResult.Text = "label3";
+            // 
+            // lblComputerResult
+            // 
+            this.lblComputerResult.AutoSize = true;
+            this.lblComputerResult.Location = new System.Drawing.Point(350, 59);
+            this.lblComputerResult.Name = "lblComputerResult";
+            this.lblComputerResult.Size = new System.Drawing.Size(41, 12);
+            this.lblComputerResult.TabIndex = 3;
+            this.lblComputerResult.Text = "label3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "VS";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(212, 179);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(53, 12);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "输还是赢";
+            // 
+            // btnStone
+            // 
+            this.btnStone.Location = new System.Drawing.Point(60, 229);
+            this.btnStone.Name = "btnStone";
+            this.btnStone.Size = new System.Drawing.Size(75, 23);
+            this.btnStone.TabIndex = 6;
+            this.btnStone.Text = "石头";
+            this.btnStone.UseVisualStyleBackColor = true;
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.Location = new System.Drawing.Point(202, 229);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(75, 23);
+            this.btnScissors.TabIndex = 7;
+            this.btnScissors.Text = "剪刀";
+            this.btnScissors.UseVisualStyleBackColor = true;
+            // 
+            // btnCloth
+            // 
+            this.btnCloth.Location = new System.Drawing.Point(352, 229);
+            this.btnCloth.Name = "btnCloth";
+            this.btnCloth.Size = new System.Drawing.Size(75, 23);
+            this.btnCloth.TabIndex = 8;
+            this.btnCloth.Text = "布";
+            this.btnCloth.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(559, 351);
+            this.Controls.Add(this.btnCloth);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnStone);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblComputerResult);
+            this.Controls.Add(this.lblPlayerResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPlayerResult;
+        private System.Windows.Forms.Label lblComputerResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnStone;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Button btnCloth;
     }
 }
 
