231057b12b1202a8fb47143ec95ee2ab7de1b4f5
Fowindy <470026339@qq.com>
2020-02-16 14:45:57 +0800

    #02.07.Computer类_电脑从移动存储设备中读取

---
+++ src/Day01_02.外部设备案例/Computer.cs
@@ -0,0 +1,19 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月16日 星期日 14:43:40
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Day01_02.外部设备案例
+{
+    /// <summary>
+    /// 07.Computer类_电脑从移动存储设备中读取
+    /// </summary>
+    public class Computer
+    {
+
+    }
+}
