32762060246307efcdc779b25eed546232aa3ad1
Fowindy <470026339@qq.com>
2020-02-17 14:58:38 +0800

    @03.01.子类继承父类,重写父类抽象方法

--- src/Day01_05_Lib_Calculate/Sub.cs
+++ src/Day01_05_Lib_Calculate/Sub.cs
@@ -9,7 +9,16 @@ using System.Xml;
 
 namespace Day01_05_Lib_Calculate
 {
-    public class Sub
+    public class Sub:JiSuan
     {
+        //构造函数必须写,不写构造函数,数值成员没有办法传进来
+        public Sub(int num1, int num2) : base(num1, num2)
+        {
+        }
+        public override int GetResult()
+        {
+            //因为给Sub类是父类的一个子类,这个属性是父类的,被子类继承
+            return this.Num1 - this.Num2;
+        }
     }
 }
