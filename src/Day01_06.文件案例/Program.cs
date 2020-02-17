99077bf62d9690a77ce0f5679a4ca1d6c2b76215
Fowindy <470026339@qq.com>
2020-02-17 16:48:03 +0800

    $Day01_06.文件案例

---
+++ src/Day01_06.文件案例/Program.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月17日 星期一 16:47:41
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Day01_06.文件案例
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
