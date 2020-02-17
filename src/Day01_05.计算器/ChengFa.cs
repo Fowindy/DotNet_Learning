a66f6f9ad74438b37a938f373b8c77b82c29019c
Fowindy <470026339@qq.com>
2020-02-17 16:41:28 +0800

    @06.02.子类继承父类,重写父类抽象方法

--- src/Day01_05.计算器/ChengFa.cs
+++ src/Day01_05.计算器/ChengFa.cs
@@ -11,8 +11,17 @@ using Day01_05_Lib_Calculate;
 
 namespace Day01_05.计算器
 {
-    public class ChengFa
+    /// <summary>
+    /// 06.02.子类继承父类,重写父类抽象方法
+    /// </summary>
+    public class ChengFa:JiSuan
     {
-
+        public ChengFa(int num1, int num2) : base(num1, num2)
+        {
+        }
+        public override int GetResult()
+        {
+            return base.Num1 * base.Num2;
+        }
     }
 }
