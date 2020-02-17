4a0804eae08bce3b3287d4deb5b42fe46d151b5e
Fowindy <470026339@qq.com>
2020-02-17 13:58:32 +0800

    @02.01.子类继承父类,重写父类抽象方法

--- src/Day01_05_Lib_Calculate/Add.cs
+++ src/Day01_05_Lib_Calculate/Add.cs
@@ -9,7 +9,20 @@ using System.Xml;
 
 namespace Day01_05_Lib_Calculate
 {
-    public class Add
+    //02.01.子类继承父类,重写父类抽象方法
+    public class Add:JiSuan
     {
+        //当你调用子类构造函数的时候,将子类num1,num2传到父类中去了
+        public Add(int num1, int num2) : base(num1, num2)
+        {
+        }
+        //父类抽象方法在子类中有关联
+        public override int GetResult()
+        {
+            //可以如下写法:
+            //return this.Num1 + this.Num2;
+            //传入父类后,正好是父类的,等价于如下写法:
+            return base.Num1 + base.Num2;
+        }
     }
 }
