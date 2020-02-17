d229d0e42ab280f6c0d24c5ee59b26e9aaf3e7ab
Fowindy <470026339@qq.com>
2020-02-17 17:36:11 +0800

    #05.添加UserFile.cs父类

---
+++ src/Day01_06.文件案例/UserFile.cs
@@ -0,0 +1,15 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月17日 星期一 17:34:45
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Day01_06.文件案例
+{
+    public class UserFile
+    {
+    }
+}
