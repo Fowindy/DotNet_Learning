d68bc6836e023258f6640b8dd162c54bd7555551
Fowindy <470026339@qq.com>
2020-02-17 18:52:32 +0800

    @04.01.XML子类继承父类,并实现抽象方法

--- src/Day01_06.文件案例/XMLFile.cs
+++ src/Day01_06.文件案例/XMLFile.cs
@@ -9,7 +9,16 @@ using System.Xml;
 
 namespace Day01_06.文件案例
 {
-    public class XMLFile
+    //04.01.XML子类继承父类,并实现抽象方法
+    public class XMLFile:UserFile
     {
+        public XMLFile(string allFileName) : base(allFileName)
+        {
+        }
+
+        public override void ShowResult()
+        {
+            Console.WriteLine("{0}:这个文件后缀为{1},可以通过浏览器查看", this.AllFileName, this.ExtensionName);
+        }
     }
 }
