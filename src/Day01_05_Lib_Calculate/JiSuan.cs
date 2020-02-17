f1edf2aea2ba3a3cabcb866b2bd7875ac80ac4e0
Fowindy <470026339@qq.com>
2020-02-17 13:43:35 +0800

    @01.02.父类需要通过构造函数拿到子类的两个成员并赋值给父类属性

--- src/Day01_05_Lib_Calculate/JiSuan.cs
+++ src/Day01_05_Lib_Calculate/JiSuan.cs
@@ -16,6 +16,14 @@ namespace Day01_05_Lib_Calculate
         //01.01.计算涉及到两个成员,定义两个成员的属性
         public int Num1 { get; set; }
         public int Num2 { get; set; }
-
+        //01.02.父类需要通过构造函数拿到子类的两个成员并赋值给父类属性
+        ///<![CDATA[[构造函数知识点]
+        ///         1.每一个类都隐式默认带一个不带参数的构造函数;
+        ///         2.当显式命名一个构造函数,则不带参数的隐式构造函数就没有了;]]>
+        public JiSuan(int num1, int num2)
+        {
+            this.Num1 = num1;
+            this.Num2 = num2;
+        }
     }
 }
