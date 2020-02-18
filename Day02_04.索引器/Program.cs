d57eeb3e55d0afa429b134e05cec4809e292b774
Fowindy <470026339@qq.com>
2020-02-18 15:50:18 +0800

    $Day02_04.索引器

---
+++ Day02_04.索引器/Program.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月18日 星期二 15:50:01
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Day02_04.索引器
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
