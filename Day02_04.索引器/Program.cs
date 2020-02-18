d1acf8862ccd74da775260906835ff21a79baabb
Fowindy <470026339@qq.com>
2020-02-18 16:32:16 +0800

    @02_01_定义Names字符数组

--- Day02_04.索引器/Program.cs
+++ Day02_04.索引器/Program.cs
@@ -15,13 +15,15 @@ namespace Day02_04.索引器
         {
             //02_实例化Person对象,只能通过.来调用,不能通过索引
             Person person = new Person();
-            person.Name = "张三";
+            //person.Name = "张三";
         }
     }
     //01_声明Person类并定义Name_Age属性
     public class Person
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
+        //public string Name { get; set; }
+        //public int Age { get; set; }
+        //02_01_定义Names字符数组
+        public string[] Names = { "yzk", "sk", "ml", "jk" };
     }
 }
