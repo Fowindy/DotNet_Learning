de8e515c1c1941e88a916f3220e4a6473355f945
Fowindy <470026339@qq.com>
2020-02-18 10:43:35 +0800

    $Day02_02.virtual_abstract_接口的区别和联系

---
+++ src/Day02_02.virtual_abstract_接口的区别和联系/Program.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月18日 星期二 10:27:42
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Day02_02.virtual_abstract_接口的区别和联系
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
