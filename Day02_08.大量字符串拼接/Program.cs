92ef024608d460eb3dfbbf44b9ff4fc9caafa0a4
Fowindy <470026339@qq.com>
2020-02-20 02:20:52 +0800

    #01_拼接一个网页

--- Day02_08.大量字符串拼接/Program.cs
+++ Day02_08.大量字符串拼接/Program.cs
@@ -13,6 +13,16 @@ namespace Day02_08.大量字符串拼接
     {
         static void Main(string[] args)
         {
+            //01_拼接一个网页
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<html>");
+            sb.Append("<head>");
+            sb.Append("<title>我的网页</title>");
+            sb.Append("</head>");
+            sb.Append("<body>");
+            sb.Append(@"<font face = ""全新硬笔行书简"" size =""7"" color = ""red"">哈哈,我又变帅了</font><br/>");
+            sb.Append("</body>");
+            sb.Append("</html>");
         }
     }
 }
