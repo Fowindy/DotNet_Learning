ccd7e58324b791bcb3f05fb038c2b1ff1dd84504
Fowindy <470026339@qq.com>
2020-02-19 09:56:15 +0800

    @06_01_比较两个字符串变量是否相等方法一_True

--- Day02_06.字符串的特性/Program.cs
+++ Day02_06.字符串的特性/Program.cs
@@ -73,6 +73,8 @@ namespace Day02_06.字符串的特性
             Console.WriteLine("--------06_将对象变成字符变量--------");
             string s1 = "张三";
             string s2 = "张三";
+            //06_01_比较两个字符串变量是否相等方法一_True
+            Console.WriteLine(s1.Equals(s2));//True
             Console.ReadKey();
         }
     }
