beb4ad15960cc4a98a66621cb434cf5c54710dd9
Fowindy <470026339@qq.com>
2020-02-19 19:32:38 +0800

    @07_03_01_先将字符串用年月日隔开

--- src/Day02_07.字符串练习/Listing07_03.练习题3_提取年月日.cs
+++ src/Day02_07.字符串练习/Listing07_03.练习题3_提取年月日.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月19日 星期三 19:31:25
+ * ==============================================================================*/
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
 
-namespace Day02_07.字符串练习
+namespace Fowindy.Day02_07.字符串练习.Listing07_03
 {
-    class Listing07_03
+    public class Listing07_03
     {
-
+        public static void Main()
+        {
+            //题目:从日期"2012年12月21日"字符串中把年月日分别提取出来,打印到控制台
+            string str = "2012年12月21日";
+            //07_03_01_先将字符串用年月日隔开
+            string[] strs = str.Split(new char[] { '年', '月', '日' }, StringSplitOptions.RemoveEmptyEntries);
+        }
     }
 }
