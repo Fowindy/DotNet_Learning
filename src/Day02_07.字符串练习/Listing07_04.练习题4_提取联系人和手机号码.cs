b305d58e0667f6a9774ba4947d82922a4c959f73
Fowindy <470026339@qq.com>
2020-02-19 21:35:35 +0800

    @07_04_02_遍历截取每一行的字符串并输出

--- src/Day02_07.字符串练习/Listing07_04.练习题4_提取联系人和手机号码.cs
+++ src/Day02_07.字符串练习/Listing07_04.练习题4_提取联系人和手机号码.cs
@@ -15,7 +15,13 @@ namespace Fowindy.Day02_07.字符串练习.Listing07_04
         public static void Main()
         {
             //07_04_01_读取文件_重点_相对上上级路径的获取方法
-            string[] lines = File.ReadAllLines("../../../相关资料/练习4.csv");
+            string[] lines = File.ReadAllLines("../../../相关资料/练习4.csv",Encoding.UTF8);
+            //07_04_02_遍历截取每一行的字符串并输出
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] str = lines[i].Split(new char[] { ',', '\"' }, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine("姓名:{0},手机号码:{1};",str[0] + str[1],str[2]);
+            }
         }
     }
 }
