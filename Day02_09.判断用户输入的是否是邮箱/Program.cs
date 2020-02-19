e2d0c37c99eed8d7422db5b3e34d1ce58e936382
Fowindy <470026339@qq.com>
2020-02-20 02:28:39 +0800

    $Day02_09.判断用户输入的是否是邮箱

---
+++ Day02_09.判断用户输入的是否是邮箱/Program.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月20日 星期四 02:28:26
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Day02_09.判断用户输入的是否是邮箱
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
