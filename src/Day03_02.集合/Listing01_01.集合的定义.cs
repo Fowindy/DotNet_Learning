f3be8efaea06456b9b5050a9a13b1b947d4de6da
Fowindy <470026339@qq.com>
2020-02-20 20:37:45 +0800

    @05_集合规律_一般情况添加元素的时候都会使用add方法

--- src/Day03_02.集合/Listing01_01.集合的定义.cs
+++ src/Day03_02.集合/Listing01_01.集合的定义.cs
@@ -14,6 +14,7 @@ namespace Fowindy.Day03_02.集合.Listing01_01
     {
         public static void Main()
         {
+            #region 非泛型集合
             //01_ArrayList集合_必须先导入命名空间_集合中存的元素的个数可以不固定
             ArrayList arrayList = new ArrayList();
             //02_arrayList.Add存进去的时候都隐式的转换成了object类型,取出来的时候必须要进行转换,比较麻烦;比较少用ArrayList
@@ -21,6 +22,8 @@ namespace Fowindy.Day03_02.集合.Listing01_01
             Hashtable ht = new Hashtable();
             ht.Add("王岳", 17786508658);
             //04_ArrayList和Hashtable属于非泛型集合_用得比较少
+            //05_集合规律_一般情况添加元素的时候都会使用add方法 
+            #endregion
         }
     }
 }
