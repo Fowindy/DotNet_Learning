c27a6d89028b3abfa6404a758a698db58541e550
Fowindy <470026339@qq.com>
2020-02-20 15:43:31 +0800

    #06_out只出不进_out传进的变量不初始化也就可以_但是必须在方法中初始化

--- src/Day02_11.ref和out的使用/Program.cs
+++ src/Day02_11.ref和out的使用/Program.cs
@@ -14,12 +14,13 @@ namespace Day02_11.ref和out的使用
         static void Main(string[] args)
         {
             //04_传进ref之前必须进行变量初始化_ref可进可出_可以往通过ref将值传进方法也可以通过ref带出来
+            //06_out只出不进_out传进的变量不初始化也就可以_但是必须在方法中初始化
             int num = 1000;
             //02_此时调用Add方法_但因为方法作用域的原因_对Main方法并没有影响
             //03_Add方法加入ref参数后_对Main函数有影响_ref可进可出_ref传进的是变量的值在栈空间对应的地址
             //Add(ref num);
             Add(out num);
-            Console.WriteLine(num);//100//1000
+            Console.WriteLine(num);//10000//100//1000
             Console.ReadKey();  
         }
         //05_声明一个Add方法_带out参数_不能构成重载_因为ref和out作用相似
