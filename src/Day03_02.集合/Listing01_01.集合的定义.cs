c523eb029c0fe17041fc5ce95cc87f74dd13506c
Fowindy <470026339@qq.com>
2020-02-20 20:31:26 +0800

    @03_Hashtable以键值对的方式存值,因为存值是object,取出时必须进行转换

--- src/Day03_02.集合/Listing01_01.集合的定义.cs
+++ src/Day03_02.集合/Listing01_01.集合的定义.cs
@@ -17,6 +17,9 @@ namespace Fowindy.Day03_02.集合.Listing01_01
             //01_ArrayList集合_必须先导入命名空间_集合中存的元素的个数可以不固定
             ArrayList arrayList = new ArrayList();
             //02_arrayList.Add存进去的时候都隐式的转换成了object类型,取出来的时候必须要进行转换,比较麻烦;比较少用ArrayList
+            //03_Hashtable以键值对的方式存值,因为存值是object,取出时必须进行转换
+            Hashtable ht = new Hashtable();
+            ht.Add("王岳", 17786508658);
         }
     }
 }
