2dd261f0101d79bde19dc5e563920d97185c8a62
Fowindy <470026339@qq.com>
2020-02-20 15:40:52 +0800

    #05_声明一个Add方法_带out参数_不能构成重载_因为ref和out作用相似

--- src/Day02_11.ref和out的使用/Program.cs
+++ src/Day02_11.ref和out的使用/Program.cs
@@ -17,14 +17,20 @@ namespace Day02_11.ref和out的使用
             int num = 1000;
             //02_此时调用Add方法_但因为方法作用域的原因_对Main方法并没有影响
             //03_Add方法加入ref参数后_对Main函数有影响_ref可进可出_ref传进的是变量的值在栈空间对应的地址
-            Add(ref num);
+            //Add(ref num);
+            Add(out num);
             Console.WriteLine(num);//100//1000
             Console.ReadKey();  
         }
-        //01_声明一个Add方法
-        public static void Add(ref int n1)
+        //05_声明一个Add方法_带out参数_不能构成重载_因为ref和out作用相似
+        public static void Add(out int num1)
         {
-            n1 = 100;
+            num1 = 10000;
         }
+        //01_声明一个Add方法
+        //public static void Add(ref int n1)
+        //{
+        //    n1 = 100;
+        //}
     }
 }
