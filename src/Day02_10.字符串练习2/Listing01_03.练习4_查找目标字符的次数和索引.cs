2c3996023cc346fff2fc923028f224c7bf734cd7
Fowindy <470026339@qq.com>
2020-02-20 14:37:04 +0800

    @01_03_01_定义目标字段_序标_次数

--- src/Day02_10.字符串练习2/Listing01_03.练习4_查找目标字符的次数和索引.cs
+++ src/Day02_10.字符串练习2/Listing01_03.练习4_查找目标字符的次数和索引.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月20日 星期四 14:19:27
+ * ==============================================================================*/
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
 
-namespace Day02_10.字符串练习2
+namespace Fowindy.Day02_10.字符串练习2.Listing01_03
 {
-    class Listing01_03
+    public class Listing01_03
     {
+        public static void Main()
+        {
+            string str = "传说在传智播客.net部门有个讲师叫小杨,小杨很帅,很纯洁,小杨是智慧和勇敢的化身.第一次听人这么说我不相信,于是我怀着试试看的心情来到了传智播客,见到了传说中的小杨。哇塞,小杨岂止是帅,简直就是帅的无法用言语来形容.";
+            //01_03_01_定义目标字段_序标_次数
+            string keyWord = "小杨";
+            int index = 0;
+            int count = 0;
+
+        }
     }
 }
