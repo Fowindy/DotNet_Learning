aace108566d7dfba20255fd86c2f9412e21d02e4
Fowindy <470026339@qq.com>
2020-02-20 20:11:49 +0800

    @02_arrayList.Add存进去的时候都隐式的转换成了object类型,取出来的时候必须要进行转换,比较麻烦;比较少用ArrayList

--- src/Day03_02.集合/Listing01_01.集合的定义.cs
+++ src/Day03_02.集合/Listing01_01.集合的定义.cs
@@ -16,6 +16,7 @@ namespace Fowindy.Day03_02.集合.Listing01_01
         {
             //01_ArrayList集合_必须先导入命名空间_集合中存的元素的个数可以不固定
             ArrayList arrayList = new ArrayList();
+            //02_arrayList.Add存进去的时候都隐式的转换成了object类型,取出来的时候必须要进行转换,比较麻烦;比较少用ArrayList
         }
     }
 }
