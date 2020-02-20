4513fe2d1a2e03ec38d399e18013fbb19d7f0280
Fowindy <470026339@qq.com>
2020-02-20 15:25:28 +0800

    #02_此时调用Add方法_但因为方法作用域的原因_对Main方法并没有影响

--- src/Day02_11.ref和out的使用/Program.cs
+++ src/Day02_11.ref和out的使用/Program.cs
@@ -13,6 +13,11 @@ namespace Day02_11.ref和out的使用
     {
         static void Main(string[] args)
         {
+            int num = 1000;
+            //02_此时调用Add方法_但因为方法作用域的原因_对Main方法并没有影响
+            Add(num);
+            Console.WriteLine(num);//1000
+            Console.ReadKey();  
         }
         //01_声明一个Add方法
         public static void Add(int n1)
