710bee3417ec6c3b078abc45c2e0bc310208d916
Fowindy <470026339@qq.com>
2020-02-20 23:11:59 +0800

    @01_遍历list1中是否存在list2中的元素_不存在则添加到list1中

--- src/Day03_02.集合/Listing01_03.案例2_去重合并集合.cs
+++ src/Day03_02.集合/Listing01_03.案例2_去重合并集合.cs
@@ -13,6 +13,19 @@ namespace Fowindy.Day03_02.集合.Listing01_03
     {
         public static void Main()
         {
+            //案例2:两个list集合;集合{'a','b','c','d','e'}和{'d','e','f','g','h'},
+            //把这两个集合去除重复合并成一个
+            List<char> list1 = new List<char> { 'a', 'b', 'c', 'd', 'e' };
+            List<char> list2 = new List<char> { 'd', 'e', 'f', 'g', 'h' };
+            for (int i = 0; i < list2.Count; i++)
+            {
+                //01_遍历list1中是否存在list2中的元素_不存在则添加到list1中
+                if (!list1.Contains(list2[i]))
+                {
+                    list1.Add(list2[i]);
+                }
+            }
+
         }
     }
 }
