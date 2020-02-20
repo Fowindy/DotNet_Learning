d36db48906a06590c0f3716d9b60160c68ed7491
Fowindy <470026339@qq.com>
2020-02-20 20:11:14 +0800

    @01_ArrayList集合_必须先导入命名空间_集合中存的元素的个数可以不固定

--- src/Day03_02.集合/Listing01_01.集合的定义.cs
+++ src/Day03_02.集合/Listing01_01.集合的定义.cs
@@ -3,6 +3,7 @@
  * 创建日期：2020年2月20日 星期四 19:33:51
  * ==============================================================================*/
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Xml;
@@ -13,6 +14,8 @@ namespace Fowindy.Day03_02.集合.Listing01_01
     {
         public static void Main()
         {
+            //01_ArrayList集合_必须先导入命名空间_集合中存的元素的个数可以不固定
+            ArrayList arrayList = new ArrayList();
         }
     }
 }
