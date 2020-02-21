d9d89c7347152bf6b36d9e1e6c94b441cc25e4bb
Fowindy <470026339@qq.com>
2020-02-21 09:07:08 +0800

    @01_往Dictionary中添加与元素

--- src/Day03_02.集合/Listing01_05.键值对集合讲解.cs
+++ src/Day03_02.集合/Listing01_05.键值对集合讲解.cs
@@ -13,6 +13,11 @@ namespace Fowindy.Day03_02.集合.Listing01_05
     {
         public static void Main()
         {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            //01_往Dictionary中添加与元素
+            dic.Add("yzk", "杨中科");
+            dic.Add("zxh", "赵小虎");
+            dic.Add("jk", "姜昆");
         }
     }
 }
