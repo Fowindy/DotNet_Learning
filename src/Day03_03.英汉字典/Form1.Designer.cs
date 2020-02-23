5ccdf2b2514d87d867aa8c95b237a6eaf66f3324
Fowindy <470026339@qq.com>
2020-02-23 10:00:12 +0800

    #01_界面搭建完成

--- src/Day03_03.英汉字典/Form1.Designer.cs
+++ src/Day03_03.英汉字典/Form1.Designer.cs
@@ -28,13 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtEnglish = new System.Windows.Forms.TextBox();
+            this.txtChinese = new System.Windows.Forms.TextBox();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEnglish
+            // 
+            this.txtEnglish.Location = new System.Drawing.Point(52, 49);
+            this.txtEnglish.Multiline = true;
+            this.txtEnglish.Name = "txtEnglish";
+            this.txtEnglish.Size = new System.Drawing.Size(367, 98);
+            this.txtEnglish.TabIndex = 0;
+            // 
+            // txtChinese
+            // 
+            this.txtChinese.Location = new System.Drawing.Point(52, 199);
+            this.txtChinese.Multiline = true;
+            this.txtChinese.Name = "txtChinese";
+            this.txtChinese.Size = new System.Drawing.Size(367, 190);
+            this.txtChinese.TabIndex = 1;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(477, 158);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(75, 23);
+            this.btnTranslate.TabIndex = 2;
+            this.btnTranslate.Text = "翻译";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.txtChinese);
+            this.Controls.Add(this.txtEnglish);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtEnglish;
+        private System.Windows.Forms.TextBox txtChinese;
+        private System.Windows.Forms.Button btnTranslate;
     }
 }
 
