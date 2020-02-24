55eddf6d4117d1a276a3d5bc7fb74c82e4feddbf
Fowindy <470026339@qq.com>
2020-02-25 07:04:55 +0800

    #03_02_调用文件流写入方法_字节数组方式_从0开始读取_读取字节数组长度

--- src/Day03_06.文件流读文件/Program.cs
+++ src/Day03_06.文件流读文件/Program.cs
@@ -23,6 +23,8 @@ namespace Day03_06.文件流读文件
             //03_通过流的方式将字符串写入文本中
             //03_01_创建一个文件流对象_文件生成路径_文件模式_文件读写方式
             FileStream fileStream = new FileStream("../../../相关资料/1.txt",FileMode.Create,FileAccess.Write);
+            //03_02_调用文件流写入方法_字节数组方式_从0开始读取_读取字节数组长度
+            fileStream.Write(buffer, 0, buffer.Length);
         }
     }
 }
