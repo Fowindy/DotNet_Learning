c3f6ab54e4019ff891bc208d9d8628b70165a269
Fowindy <470026339@qq.com>
2020-02-25 06:32:38 +0800

    %Day03_06.文件流读文件

---
+++ src/Day03_06.文件流读文件/Program.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月25日 星期二 06:32:22
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Day03_06.文件流读文件
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
