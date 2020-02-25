e1118c488fc240e1c189f3cd3c1ecb86702aa014
Fowindy <470026339@qq.com>
2020-02-25 08:28:00 +0800

    %Day03_07.复制视频文件

---
+++ src/Day03_07.复制视频文件/Program.cs
@@ -0,0 +1,18 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月25日 星期二 08:27:44
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Day03_07.复制视频文件
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
