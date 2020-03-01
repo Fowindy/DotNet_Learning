2e4d7f4faf00ea0ebd712e578d7ea4dd24b35a23
Fowindy <470026339@qq.com>
2020-03-02 07:19:26 +0800

    %Day03_08.StreamReader和StreamWriter

---
+++ src/Day03_08.StreamReader和StreamWriter/Program.cs
@@ -0,0 +1,23 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年2月28日 星期五 23:46:22
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Day03_08.StreamReader和StreamWriter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader())
+            {
+                 
+            }
+        }
+    }
+}
