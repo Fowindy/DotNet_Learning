8c73aaea7443c90fd48c239d7fa47a6ef9a4102b
Fowindy <470026339@qq.com>
2020-03-05 11:41:35 +0800

    %Day03_09.File类的使用

---
+++ src/Day03_09.File类的使用/Program.cs
@@ -0,0 +1,19 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年3月5日 星期四 11:41:21
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Day03_09.File类的使用
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
