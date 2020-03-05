f4b58d7b99aba1d7f9c79c21c53f350d780f0aa6
Fowindy <470026339@qq.com>
2020-03-05 11:38:39 +0800

    #03_02_大文件的读写使用FileStream_较复杂

--- src/Day03_08.StreamReader和StreamWriter/Program.cs
+++ src/Day03_08.StreamReader和StreamWriter/Program.cs
@@ -40,6 +40,7 @@ namespace Day03_08.StreamReader和StreamWriter
             }
             //03_读写文件方式总结
             //03_01_小文件的读写使用File
+            //03_02_大文件的读写使用FileStream_较复杂
             Console.ReadKey();
         }
     }
