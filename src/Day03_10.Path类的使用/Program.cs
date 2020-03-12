855b66243cd922f1426dd8a3c94f6a23e7656084
Fowindy <470026339@qq.com>
2020-03-12 08:21:02 +0800

    $02_Combine合并路径的_反射的时候会使用到

--- src/Day03_10.Path类的使用/Program.cs
+++ src/Day03_10.Path类的使用/Program.cs
@@ -16,8 +16,12 @@ namespace Day03_10.Path类的使用
         {
             //01_ChangeExtension更改路径字符串的后缀名
             string path = @"c:\1.exe";
-            path = Path.ChangeExtension(path, ".txt");
+            //path = Path.ChangeExtension(path, ".txt");
+            //02_Combine合并路径的_反射的时候会使用到
+            path = Path.Combine(@"c:\电影", @"视频\1.txt");
             Console.WriteLine(path);
+            //01_c:\1.txt
+            //02_c:\电影\视频\1.txt
             Console.ReadKey();
         }
     }
