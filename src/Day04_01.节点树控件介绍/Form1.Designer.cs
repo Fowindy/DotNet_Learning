be9f888031848d9bdfe5ea03928e33e17172ecdf
Fowindy <470026339@qq.com>
2020-09-16 09:50:08 +0800

    Revert "  添加根节点数_属性添加的方式_弊端写死了无法更改"
    This reverts commit 47a24507c80fdd8db1386ff2b633b591da7502f5.

--- src/Day04_01.节点树控件介绍/Form1.Designer.cs
+++ src/Day04_01.节点树控件介绍/Form1.Designer.cs
@@ -28,17 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("C盘");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("D盘");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("我的电脑", new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2});
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("控制面板");
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("本地网络");
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("无线网络");
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("网上邻居", new System.Windows.Forms.TreeNode[] {
-            treeNode5,
-            treeNode6});
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
@@ -46,24 +35,6 @@
             // 
             this.treeView1.Location = new System.Drawing.Point(51, 57);
             this.treeView1.Name = "treeView1";
-            treeNode1.Name = "节点4";
-            treeNode1.Text = "C盘";
-            treeNode2.Name = "节点5";
-            treeNode2.Text = "D盘";
-            treeNode3.Name = "节点0";
-            treeNode3.Text = "我的电脑";
-            treeNode4.Name = "节点1";
-            treeNode4.Text = "控制面板";
-            treeNode5.Name = "节点6";
-            treeNode5.Text = "本地网络";
-            treeNode6.Name = "节点7";
-            treeNode6.Text = "无线网络";
-            treeNode7.Name = "节点2";
-            treeNode7.Text = "网上邻居";
-            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode3,
-            treeNode4,
-            treeNode7});
             this.treeView1.Size = new System.Drawing.Size(270, 315);
             this.treeView1.TabIndex = 0;
             // 
