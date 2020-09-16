a32847c8a3b13ce883d3242dd4057121d042cb97
Fowindy <470026339@qq.com>
2020-09-16 09:43:15 +0800

    Day04_01.节点树控件介绍_项目创建

---
+++ src/Day04_01.节点树控件介绍/Program.cs
@@ -0,0 +1,26 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年9月16日 星期三 09:29:39
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day04_01.节点树控件介绍
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
