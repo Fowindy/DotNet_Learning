d02dcdcf3db552fb934545a02fe34b4e8f8c595d
Fowindy <470026339@qq.com>
2020-09-22 19:55:03 +0800

    Listing04_05.获取目标网页所有页面邮箱

---
+++ src/Day04_05.获取目标网页所有页面邮箱/Listing04_05.获取目标网页所有页面邮箱.cs
@@ -0,0 +1,30 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月22日 星期二 19:17:12
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月22日 星期二 19:17:12
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Fowindy.Day04_05.获取目标网页所有页面邮箱.Listing04_05
+{
+    public class Listing04_05
+    {
+        public static void Main()
+        {
+        }
+    }
+}
