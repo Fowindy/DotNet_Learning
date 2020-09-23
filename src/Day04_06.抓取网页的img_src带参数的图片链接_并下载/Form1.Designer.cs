57a02ddd3ae21a5ab7f21775325a5afa6496cc50
Fowindy <470026339@qq.com>
2020-09-24 06:50:43 +0800

    Day04_06.抓取网页的img_src带参数的图片链接_并下载

---
+++ src/Day04_06.抓取网页的img_src带参数的图片链接_并下载/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Day04_06.抓取网页的img_src带参数的图片链接_并下载
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bGrab = new System.Windows.Forms.Button();
+            this.tbUrlPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtLog = new System.Windows.Forms.RichTextBox();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bGrab);
+            this.groupBox1.Controls.Add(this.tbUrlPath);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(44, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(535, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "抓取图片";
+            // 
+            // bGrab
+            // 
+            this.bGrab.Location = new System.Drawing.Point(441, 44);
+            this.bGrab.Name = "bGrab";
+            this.bGrab.Size = new System.Drawing.Size(75, 23);
+            this.bGrab.TabIndex = 2;
+            this.bGrab.Text = "抓取";
+            this.bGrab.UseVisualStyleBackColor = true;
+            this.bGrab.Click += new System.EventHandler(this.bGrab_Click);
+            // 
+            // tbUrlPath
+            // 
+            this.tbUrlPath.Location = new System.Drawing.Point(68, 46);
+            this.tbUrlPath.Name = "tbUrlPath";
+            this.tbUrlPath.Size = new System.Drawing.Size(367, 21);
+            this.tbUrlPath.TabIndex = 1;
+            this.tbUrlPath.Text = "http://pic.netbian.com/4kmeinv/index.html";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rtLog);
+            this.groupBox2.Controls.Add(this.tbTitle);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(44, 158);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(535, 280);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "结果";
+            // 
+            // rtLog
+            // 
+            this.rtLog.Location = new System.Drawing.Point(68, 58);
+            this.rtLog.Name = "rtLog";
+            this.rtLog.Size = new System.Drawing.Size(367, 216);
+            this.rtLog.TabIndex = 2;
+            this.rtLog.Text = "";
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(68, 18);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(367, 21);
+            this.tbTitle.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "标题";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bGrab;
+        private System.Windows.Forms.TextBox tbUrlPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox rtLog;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ src/Day04_06.抓取网页的img_src带参数的图片链接_并下载/Program.cs
@@ -0,0 +1,26 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年9月23日 星期三 13:45:21
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day04_06.抓取网页的img_src带参数的图片链接_并下载
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
