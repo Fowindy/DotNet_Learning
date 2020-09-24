bf8c32e82aee24b361b38d83b445fde5c49967f1
Fowindy <470026339@qq.com>
2020-09-24 12:08:38 +0800

    Listing04_08_01.读取xml文件的内容_创建

---
+++ src/Day04_08.XML/Listing04_08_01.读取xml文件的内容.cs
@@ -0,0 +1,28 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月24日 星期四 12:07:56
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月24日 星期四 12:07:56
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Day04_08.XML.Listing04_08_01
+{
+    public class Listing04_08_01
+    {
+        public static void Main()
+        {
+        }
+    }
+}
