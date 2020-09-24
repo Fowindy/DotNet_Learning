6825c18b407b7b136e28158d9568a2812747e83a
Fowindy <470026339@qq.com>
2020-09-25 00:04:07 +0800

    Day05_03.xml文件登录_界面完成

---
+++ src/Day05_03.xml文件登录/Form1.cs
@@ -0,0 +1,25 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年9月24日 星期四 23:57:16
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Day05_03.xml文件登录
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
