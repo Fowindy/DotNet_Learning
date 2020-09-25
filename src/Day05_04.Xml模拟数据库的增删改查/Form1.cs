82bbed67eb0529a853f526234093c3f94ef0db62
Fowindy <470026339@qq.com>
2020-09-25 10:21:42 +0800

    Day05_04.Xml模拟数据库的增删改查_界面初步完成

---
+++ src/Day05_04.Xml模拟数据库的增删改查/Form1.cs
@@ -0,0 +1,29 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年9月25日 星期五 07:36:24
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Day05_04.Xml模拟数据库的增删改查
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
