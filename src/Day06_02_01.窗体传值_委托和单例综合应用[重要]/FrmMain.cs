9358fb99798f3de7c0bcc232083bbf839ea272b5
Fowindy <470026339@qq.com>
2020-09-27 17:24:40 +0800

    Day06_02_01.窗体传值_委托和单例综合应用[重要]_界面设计完成

---
+++ src/Day06_02_01.窗体传值_委托和单例综合应用[重要]/FrmMain.cs
@@ -0,0 +1,35 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月27日 星期日 16:04:17
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月27日 星期日 16:04:17
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Day06_02_01.窗体传值_委托和单例综合应用_重要_
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Day06_02_01.窗体传值_委托和单例综合应用[重要]/Program.cs
@@ -0,0 +1,35 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月27日 星期日 16:04:17
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月27日 星期日 16:04:17
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day06_02_01.窗体传值_委托和单例综合应用_重要_
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmMain());
+        }
+    }
+}
