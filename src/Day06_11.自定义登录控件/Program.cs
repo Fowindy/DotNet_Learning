31d425a15c3a40d2921f55938d18cb7bea9b6dc5
Fowindy <470026339@qq.com>
2020-09-28 18:25:26 +0800

    Day06_11.自定义登录控件_创建

---
+++ src/Day06_11.自定义登录控件/Program.cs
@@ -0,0 +1,35 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月28日 星期一 18:24:58
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月28日 星期一 18:24:58
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day06_11.自定义登录控件
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
