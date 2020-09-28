52bc33cfd7e88892c276df71a0b9b7cf024d0ef6
Fowindy <470026339@qq.com>
2020-09-28 17:07:01 +0800

    委托的弊端2

--- src/Day06_09.委托三连击/Form1.cs
+++ src/Day06_09.委托三连击/Form1.cs
@@ -38,7 +38,9 @@ namespace Day06_09.委托三连击
         //如果最开始点这个按钮,委托的实例方法会变成这个,然后自定义控件按钮点三次将会显示Say的内容
         private void button1_Click(object sender, EventArgs e)
         {
-            utc._mdl = Say;
+            //委托的弊端2
+            //如果是+号,点击按钮1则会委托加上Say这个方法，然后自定义控件按钮点三次将会显示两个方法的内容
+            utc._mdl += Say;
         }
 
         private void Say()
