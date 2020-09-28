b5ca3c06f355a073e8275a468c082744b489d555
Fowindy <470026339@qq.com>
2020-09-28 17:15:55 +0800

    Day06_10.委托三连击_事件实现_项目创建

---
+++ src/Day06_10.委托三连击_事件实现/Form1.cs
@@ -0,0 +1,35 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月28日 星期一 17:14:09
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月28日 星期一 17:14:09
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Day06_10.委托三连击_事件实现
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
