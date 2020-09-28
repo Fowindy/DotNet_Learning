7bcd466d5692b563bed38e07a325199dfa34ab7d
Fowindy <470026339@qq.com>
2020-09-28 08:43:54 +0800

    有参无参Lamda表达式的高级用法

--- src/Day06_06.匿名方法_Lamda表达式/Listing06_06_03.匿名函数_Lamda_有参数有返回值.cs
+++ src/Day06_06.匿名方法_Lamda表达式/Listing06_06_03.匿名函数_Lamda_有参数有返回值.cs
@@ -24,10 +24,18 @@ namespace Fowindy.Day06_06.匿名方法_Lamda表达式.Listing06_06_03
     public class Listing06_06_03
     {
         public delegate int MyDel(int num1, int num2);
+        public delegate int MyDelNew(int n1, int n2, int n3);
         public static void Main()
         {
-            MyDel myDel = (int num1, int num2) => { return num1 - num2; };
+            MyDel myDel = (num1, num2) => { return num1 - num2; };
             int result = myDel(15, 25);
+            Console.WriteLine(result);
+            Console.WriteLine("----------------高级方法----------------");
+            T1((x, y, z) =>  x + y - z);//0
+        }
+        public static void T1(MyDelNew add)
+        {
+            int result = add(10,20,30);
             Console.WriteLine(result);
         }
     }
