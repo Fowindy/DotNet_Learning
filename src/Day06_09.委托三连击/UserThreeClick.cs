9fe85e2bba314aec8cf8be0dbdb16fbe1d6e7466
Fowindy <470026339@qq.com>
2020-09-28 14:46:09 +0800

    Day06_09.委托三连击_自定义声明的委托

---
+++ src/Day06_09.委托三连击/UserThreeClick.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day06_09.委托三连击
+{
+    /// <summary>
+    /// 自定义声明的委托
+    /// </summary>
+    public delegate void MyDelegate();
+    public partial class UserThreeClick : UserControl
+    {
+        public UserThreeClick()
+        {
+            InitializeComponent();
+        }
+        int n = 0;
+        public MyDelegate _mdl;
+        private void ThreeClick_Click(object sender, EventArgs e)
+        {
+            n++;
+            if (n == 3)
+            {
+                if (this._mdl != null)
+                {
+                    this._mdl();
+                }
+            }
+        }
+    }
+}
