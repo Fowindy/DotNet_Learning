5db26a2bd3776e654aff659e64f6104fa365fce2
Fowindy <470026339@qq.com>
2020-09-29 07:20:53 +0800

    拿到类中所有的方法

---
+++ src/Day06_12.程序集/Listing06_12_01.获取程序集的信息.cs
@@ -0,0 +1,53 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月29日 星期二 00:10:42
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月29日 星期二 00:10:42
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+using System.Reflection;
+
+namespace Fowindy.Day06_12.程序集.Listing06_12_01
+{
+    public class Listing06_12_01
+    {
+        public static void Main()
+        {
+            Person person = new Person();
+            //获取person的Type
+            Type type = person.GetType();
+            //获取当前type对象的所有的公共的方法
+            MethodInfo[] meths = type.GetMethods();
+            //遍历所有的方法
+            for (int i = 0; i < meths.Length; i++)
+            {
+                //打印方法的名字
+                Console.WriteLine(meths[i].Name);
+            }
+        }
+    }
+    public class Person
+    {
+        //字段就是变量
+        private int _age;
+        //属性就是方法;Get方法和Set方法
+        public int Age { get => _age; set => _age = value; }
+        public void Say()
+        {
+            Console.WriteLine("我是人,需要吃饭!");
+        }
+    }
+}
