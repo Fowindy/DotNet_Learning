9ec7122dece6109aa55f2c5c879a581ac844e80d
Fowindy <470026339@qq.com>
2020-09-29 09:34:56 +0800

    加入Student类

---
+++ src/Day06_13.获取程序集/Student.cs
@@ -0,0 +1,27 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月29日 星期二 09:31:33
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月29日 星期二 09:31:33
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Day06_13.获取程序集
+{
+    public class Student:Person
+    {
+    }
+}
