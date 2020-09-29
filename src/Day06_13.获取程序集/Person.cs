03cb6ea0e063ba296f30a49ff5db4bdb13cec8ae
Fowindy <470026339@qq.com>
2020-09-29 08:36:38 +0800

    Day06_13.获取程序集_类库项目完成

---
+++ src/Day06_13.获取程序集/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day06_13.获取程序集
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public void Show()
+        {
+            Console.WriteLine("人类");
+        }
+    }
+}
