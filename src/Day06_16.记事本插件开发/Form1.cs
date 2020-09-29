51fd42a4f6bf195454a0ada41ff9604c92216133
Fowindy <470026339@qq.com>
2020-09-29 17:29:53 +0800

    Day06_16.记事本插件开发_接口(转大写功能完成)

--- src/Day06_16.记事本插件开发/Form1.cs
+++ src/Day06_16.记事本插件开发/Form1.cs
@@ -23,6 +23,7 @@ using System.Windows.Forms;
 using System.IO;
 using System.Xml.Linq;
 using System.Reflection;
+using Day06_17.IEditPlus接口;
 
 namespace Day06_16.记事本插件开发
 {
@@ -43,18 +44,50 @@ namespace Day06_16.记事本插件开发
             //---Assembly.GetExecutingAssembly().Location---包含可行性文件的全路径
             //---Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)---获取当前执行文件所在文件夹路径
             string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Lib");//拼组Lib路径
-            //1.2.搜索Lib路径中的dll文件
+            //1.2.搜索Lib路径中的dll文件(插件)
             string[] files = Directory.GetFiles(path, "*.dll");
             //1.3.遍历所有dll文件
             for (int i = 0; i < files.Length; i++)
             {
-                //1.3.1.获取该dll文件中所有的程序集
+                //1.3.1.获取该dll文件中所有的程序集(插件)
                 Assembly ass = Assembly.LoadFile(files[i]);
                 //1.3.2.获取该dll文件所有的types
                 Type[] types = ass.GetTypes();
                 #region 1.3.3.插件接口验证--->2.制作IEditPlus接口类
+                //1.3.4.获取接口的类型(接口)
+                Type iEdi = typeof(IEditPlus);
+                //1.3.5.再遍历插件中所有的类,判断是否遵循主程序的接口规范
+                for (int j = 0; j < types.Length; j++)
+                {
+                    //1.3.6.如果插件类继承接口类并且不是抽象类
+                    if (iEdi.IsAssignableFrom(types[i]) && !types[j].IsAbstract)
+                    {
+                        //1.3.7.将此插件转换成接口对象
+                        IEditPlus plus = (IEditPlus)Activator.CreateInstance(types[j]);
+                        //1.3.8.为menuStrip1的Format菜单增加下拉按钮,并返回按钮对象
+                        ToolStripItem tsp = Format.DropDownItems.Add(plus.Name);
+                        //1.3.9.把插件转换的接口对象存储到按钮的tag属性中
+                        tsp.Tag = plus;
+                        //1.3.10.单击事件-->1.4.按钮点击事件
+                        tsp.Click += new EventHandler(tsp_Click);
+                    }
+                }
                 #endregion
             }
+        }
+        /// <summary>
+        /// 1.4.按钮点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsp_Click(object sender, EventArgs e)
+        {
+            //1.4.1.子类按钮对象转换成父类
+            ToolStripItem tsp = sender as ToolStripItem;
+            //1.4.2.把插件对象转换成接口分类对象
+            IEditPlus iep = tsp.Tag as IEditPlus;
+            //1.4.3.把插件处理后的结果显示在界面中
+            tbContent.Text = iep.StringFormat(tbContent);
         }
     }
 }
