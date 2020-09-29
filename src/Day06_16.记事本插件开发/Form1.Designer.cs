21947d462c33ac30f2c257b2644c9820dee1d513
Fowindy <470026339@qq.com>
2020-09-29 13:05:19 +0800

    遍历dll文件完成

--- src/Day06_16.记事本插件开发/Form1.Designer.cs
+++ src/Day06_16.记事本插件开发/Form1.Designer.cs
@@ -101,6 +101,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "记事本";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
--- src/Day06_16.记事本插件开发/Form1.cs
+++ src/Day06_16.记事本插件开发/Form1.cs
@@ -22,6 +22,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Xml.Linq;
+using System.Reflection;
 
 namespace Day06_16.记事本插件开发
 {
@@ -30,6 +31,30 @@ namespace Day06_16.记事本插件开发
         public Form1()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// 1.记事本Load事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //1.1.窗体加载,扫描可执行路径/Lib文件路径的dll文件
+            //---Assembly.GetExecutingAssembly().Location---包含可行性文件的全路径
+            //---Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)---获取当前执行文件所在文件夹路径
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Lib");//拼组Lib路径
+            //1.2.搜索Lib路径中的dll文件
+            string[] files = Directory.GetFiles(path, "*.dll");
+            //1.3.遍历所有dll文件
+            for (int i = 0; i < files.Length; i++)
+            {
+                //1.3.1.获取该dll文件中所有的程序集
+                Assembly ass = Assembly.LoadFile(files[i]);
+                //1.3.2.获取该dll文件所有的types
+                Type[] types = ass.GetTypes();
+                #region 1.3.3.插件接口验证--->2.制作IEditPlus接口类
+                #endregion
+            }
         }
     }
 }
